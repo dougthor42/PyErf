bfa6c86abacd69046191a3796b21d98ec4079da9
Douglas Thor <dthor@transphormusa.com>
2017-02-27 12:48:23 -0800

    updated setup.py, fixed unit tests.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ classifiers = [
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Topic :: Scientific/Engineering :: Mathematics",
